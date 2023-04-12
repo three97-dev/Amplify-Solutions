c7d2532c694537e9a0ae8e01da064e0c0fde390e
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2023-04-12 16:45:18 +0000

    Fix hubspot meetings embed

--- src/components/booking/MeetingEmbedSection.jsx
+++ src/components/booking/MeetingEmbedSection.jsx
@@ -6,10 +6,14 @@ const MeetingEmbedSection = () => {
   return (
     <Container>
       <div className="-mx-[12px] min-[360px]:mx-0 pt-20">
-        <div
-          className="meetings-iframe-container"
-          data-src="https://meetings.hubspot.com/mkennedy17/mike-meeting-link?embed=true"
-        ></div>
+        <iframe
+          title={"hubspot"}
+          style={{
+            width: "100%",
+            height: "756px",
+          }}
+          src={"https://meetings.hubspot.com/mkennedy17/mike-meeting-link?embed=true"}
+        />
       </div>
     </Container>
   );
--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -6,17 +6,10 @@ import Seo from "../seo/Seo";
 
 import Hero from "../components/Hero";
 import MeetingEmbedSection from "../components/booking/MeetingEmbedSection";
-import { Helmet } from "react-helmet";
 
 const BookingPage = ({ data }) => {
   return (
     <Layout>
-      <Helmet>
-        <script
-          type="text/javascript"
-          src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"
-        ></script>
-      </Helmet>
       <Seo title="Booking" />
       <Hero
         title={data.contentfulBookingPage.heroTitle}
