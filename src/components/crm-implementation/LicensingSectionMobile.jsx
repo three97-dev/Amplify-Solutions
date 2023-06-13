2752dcff0feaa1a5433a8b663d994c4796d5e9e9
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2023-06-13 09:51:12 +0000

    Licensing section mobile redesign

--- src/components/crm-implementation/LicensingSectionMobile.jsx
+++ src/components/crm-implementation/LicensingSectionMobile.jsx
@@ -3,33 +3,43 @@ import React from "react";
 import Container from "../Container";
 import RRichtext from "../RichtextRenderer";
 
+const TableRow = ({ label, value }) => {
+  return (
+    <tr className="border-b-[3px] border-light-blue">
+      <td className="border-r-[3px] border-light-blue">
+        <h3 className="!text-[16px] py-2 pr-4">{label}</h3>
+      </td>
+      <td align="center" className="border-r-[3px] border-light-blue">
+        <p className="px-4 py-2">{value}</p>
+      </td>
+    </tr>
+  );
+};
+
 const LicensingSectionMobile = ({ title, subtitle, column1Title, column2Title, column3Title, rows }) => {
   return (
     <Container>
-      <div className="pb-20">
+      <div className="pb-[95px]">
         <RRichtext data={title} config={{ h2: "text-[32px] md:text-[38px] leading-[120%]" }} />
         <RRichtext data={subtitle} config={{ h3: "text-[22px] leading-[29px]" }} className="mt-4" />
-        <div className="grid grid-cols-3 gap-x-2 sm:gap-x-3 pt-4 text-center">
-          <h3 className="max-[500px]:!text-[16px]">{column1Title}</h3>
-          <h3 className="max-[500px]:!text-[16px]">{column2Title}</h3>
-          <h3 className="max-[500px]:!text-[16px]">{column3Title}</h3>
-        </div>
-        {rows.map((row, index) => (
-          <div key={index}>
-            <h4 className="font-medium font-poppins py-4">{row.label}</h4>
-            <div className="grid grid-cols-3 md:text-center">
-              <div className="flex justify-center items-center border-r-[3px] border-light-blue">
-                <p className="!text-[12px] md:!text-[14px] pr-1">{row.light}</p>
-              </div>
-              <div className="flex justify-center items-center border-r-[3px] border-light-blue">
-                <p className="!text-[12px] md:!text-[14px] px-1">{row.professional}</p>
-              </div>
-              <div className="flex justify-center items-center">
-                <p className="!text-[12px] md:!text-[14px] pl-1">{row.enterprise}</p>
-              </div>
-            </div>
-          </div>
-        ))}
+        <table className="w-full border-collapse border-spacing-0">
+          <tbody>
+            {rows.map((row, index) => (
+              <React.Fragment key={index}>
+                <tr className="h-[40px]">
+                  <td colSpan={2}>
+                    <div className="text-[22px] leading-[37px] py-[21px] pr-4 font-medium font-poppins">
+                      {row.label}
+                    </div>
+                  </td>
+                </tr>
+                <TableRow label={column1Title} value={row.light} />
+                <TableRow label={column2Title} value={row.professional} />
+                <TableRow label={column3Title} value={row.enterprise} />
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
       </div>
     </Container>
   );
